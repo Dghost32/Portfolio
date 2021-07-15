14e903d5df09a48e681eb236e19fa6208dd24b93
Carlos Jimenez <cajimenezca@unal.edu.co>
2021-07-15 18:18:27 -0500

    cleaner code in navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,36 +5,40 @@ import "../styles/Navigation.css";
 const Navigation = () => {
   return (
     <Dropdown>
-      {/* button */}
-      <Dropdown.Toggle id="nav" className="nav-btn txt-sm txt-dark">
-        <NavMenuIcon />
-      </Dropdown.Toggle>
-      {/* menu */}
-      <Dropdown.Menu id="nav-menu-container" className="nav-menu-container">
-        <NavMenuItem to="main-page">
-          <p>Main Page</p>
-        </NavMenuItem>
-        <NavMenuItem to="whoami">
-          <p>Whoami</p>
-        </NavMenuItem>
-        <NavMenuItem to="projects">
-          <p>Projects</p>
-        </NavMenuItem>
-        <NavMenuItem to="skills">
-          <p>Skills</p>
-        </NavMenuItem>
-        <hr
-          style={{ height: "1px", backgroundColor: "#c2185b", border: "none" }}
-        />
-        <NavMenuItem to="contact">
-          <p>Contact</p>
-        </NavMenuItem>
-      </Dropdown.Menu>
+      <NavButton />
+      <NavMenu />
     </Dropdown>
   );
 };
 
 export default Navigation;
+
+const NavButton = () => (
+  <Dropdown.Toggle id="nav" className="nav-btn txt-sm txt-dark">
+    <NavMenuIcon />
+  </Dropdown.Toggle>
+);
+
+const NavMenu = () => (
+  <Dropdown.Menu id="nav-menu-container" className="nav-menu-container">
+    <NavMenuItem to="main-page">
+      <p>Main Page</p>
+    </NavMenuItem>
+    <NavMenuItem to="whoami">
+      <p>Whoami</p>
+    </NavMenuItem>
+    <NavMenuItem to="projects">
+      <p>Projects</p>
+    </NavMenuItem>
+    <NavMenuItem to="skills">
+      <p>Skills</p>
+    </NavMenuItem>
+    <hr style={{ height: "1px", backgroundColor: "#c2185b", border: "none" }} />
+    <NavMenuItem to="contact">
+      <p>Contact</p>
+    </NavMenuItem>
+  </Dropdown.Menu>
+);
 
 const NavMenuIcon = () => (
   <>
